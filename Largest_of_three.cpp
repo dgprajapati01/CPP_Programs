3ed5ab5a6e619e3b2857e1084ff468493d05a529
dgprajapati01 <dhyey.prajapati52@gmail.com>
2026-03-11 22:04:41 +0530

    Largest of Three Numbers

--- Largest_of_three.cpp
+++ Largest_of_three.cpp
@@ -4,20 +4,20 @@ using namespace std;
 void largest(int &a, int &b, int &c)
 {
     if (a >=b && a >=c)
-    cout<< "Largest Number is : "<< a;
+    cout<< "Largest Number is : \n"<< a;
     
         else if (b >=a && b >=c)
-        cout<<" Largest Number Is : "<< b;
+        cout<<" Largest Number Is :\n "<< b;
 
             else
-            cout<<"Largest Number Is :"<< c;
+            cout<<"Largest Number Is :\n"<< c;
 
 }
 
 int main()
 {
     int a, b, c;
-    cout<<"Enter Three Integer :";
+    cout<<"Enter Three Integer :\n";
     cin>> a >> b >> c;
 
     largest (a,b,c);
