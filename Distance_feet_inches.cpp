e57f761abdc1ac9be2efdd4dde458ba6f12a9c33
dgprajapati01 <dhyey.prajapati52@gmail.com>
2026-03-16 21:09:05 +0530

    Distance with Feet and Inches

---
+++ Distance_feet_inches.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+class DISTANCE{
+    private:
+    int feet;
+    int inches;
+
+        public:
+        void input()
+        {
+            cout<<"Enter Feet : ";
+            cin>> feet;
+            cout<< "Enter inches : ";
+            cin>> inches;
+        }
+
+        void display()
+        {
+            cout<< feet << " feet " << inches << " inches " << endl;
+        }
+
+        DISTANCE add(DISTANCE d1, DISTANCE d2)
+
+        {
+            DISTANCE temp;
+            temp.feet = d1.feet + d2.feet;
+            temp.inches =d1.inches + d2.inches;
+
+            if(temp.inches >= 12)
+            {
+                temp.feet += temp.inches / 12;
+                temp.inches = temp.inches % 12;
+            }
+
+            return temp;
+        }
+};
+
+
+int main()
+{
+    DISTANCE d1, d2, sum;
+    
+    cout << "Enter First Distance : \n";
+    d1. input();
+
+    cout << "Enter Second Distance : \n";
+    d2.input();
+
+    sum = sum.add(d1, d2);
+
+    cout<< "Sum Of Distances = ";
+    sum.display();
+
+    return 0;
+
+}
