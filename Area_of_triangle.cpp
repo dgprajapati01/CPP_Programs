a49fd91d7dd5fdee1daac54a93635070d9a2fc6e
dgprajapati01 <dhyey.prajapati52@gmail.com>
2026-05-12 10:55:13 +0530

    Area Calculation

---
+++ Area_of_triangle.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+using namespace std;
+
+class triangle{
+private :
+
+    float base, height, area;
+
+    public:
+    void
+double area(double base, double height):
+return 0.5 * base * height; 
+
+}
+
