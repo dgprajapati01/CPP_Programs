17dfa24b30908421b0b575381145d152888f3d5f
dgprajapati01 <dhyey.prajapati52@gmail.com>
2026-05-15 12:16:13 +0530

    Length of str

---
+++ Basic_programs/Length_of_String.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    string str = "Dhyey G Prajapati";
+    
+    cout << "Length " << str.length() << endl;
+    
+    return 0;
+}
+/*
+g++ Lenth_of_String.cpp -o Lengthstr
+
+D:\2. Dhyey Sem 2\CPP_Programs\Basic_programs>Lengthstr.exe
+Length 17
+*/
