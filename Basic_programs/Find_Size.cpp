4a5f4f25db07ce21d1f9330974c8fdf0f3223686
dgprajapati01 <dhyey.prajapati52@gmail.com>
2026-05-19 10:21:04 +0530

    Find size Manually

---
+++ Basic_programs/Find_Size.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int arr[] = {10, 20, 30, 40, 50};
+
+    int size = 5;
+
+    cout << "Size of array = " << size << endl;
+
+    return 0;
+}
+
+/*
+
+g++ Find_Size.cpp -o Findsize
+
+D:\2. Dhyey Sem 2\CPP_Programs\Basic_programs>Findsize.exe
+Size of array = 5
+
+*/
