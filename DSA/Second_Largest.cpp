e7a0f020931dcac5fe12cd3f283b3928af26e65a
dgprajapati01 <dhyey.prajapati52@gmail.com>
2026-06-08 23:36:04 +0530

    Second Largest Elements

---
+++ DSA/Second_Largest.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+
+    int print2largest(int arr[], int n) {
+
+        int ans = -1;
+        for(int i=0; i<n; i++) {
+        
+            if(arr[i]>ans)
+                ans = arr[i];
+        }
+
+        int second = -1;
+
+        for (int i=0; i<n; i++)
+        {
+            if(arr[i]!=ans)
+                second = max(second, arr[i]);
+        };
+
+        return second;
+    }
+    
+};
